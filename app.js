4c60fc23fdb1e97c4ee7c86b98565f950eb0ba19
fresh5447 <doug@kosmojo.com>
2017-05-24 11:23:30 -0600

    add bears to vue display

--- app.js
+++ app.js
@@ -4,17 +4,24 @@ function Bear(name, color, img){
   this.img = img;
 }
 
-var bear = new Bear('Monty', "Brown", "http://geology.com/stories/13/bear-areas/grizzly-bear.jpg")
-var bear2 = new Bear('Yogi', "Black", "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwjC0pC5gInUAhUH0WMKHVWbDK8QjBwIBA&url=http%3A%2F%2Fweknowyourdreams.com%2Fimages%2Fbear%2Fbear-05.jpg&psig=AFQjCNEIInlXbIIE6gvJTzbXvSV0qwxglA&ust=1495731570562542")
-var bear3 = new Bear('BooBoo', "Black And White", "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=imgres&cd=&ved=0ahUKEwjCkcS_gInUAhUN0mMKHTriCSYQjBwIBA&url=http%3A%2F%2Flindelof.nu%2Fwp-content%2Fuploads%2F2015%2F02%2Fbjornbrum-220x150.jpg&psig=AFQjCNGfruABJK_-Nrd1MF0nekgQOI-YOA&ust=1495731584201404")
+var bear = new Bear('Monty', "Brown", "http://images.nationalgeographic.com/wpf/media-live/photos/000/005/cache/grizzly-bear_566_600x450.jpg")
+var bear2 = new Bear('Yogi', "Black", "https://nicodemasplusthree.files.wordpress.com/2015/05/grizzly-bear-1.jpg?w=565&h=376")
+var bear3 = new Bear('BooBoo', "Black And White", "http://www.montanaoutdoor.com/wp-content/uploads/2016/09/Screen-Shot-2016-09-28-at-7.24.38-PM.png")
 var bear4 = new Bear('Martha', "Black", "http://www.unilad.co.uk/wp-content/uploads/2015/11/Screen-Shot-2015-11-30-at-19.20.55.png")
-var bear5 = new Bear('Bertha', "Brown", "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwj78rjbgInUAhVhVWMKHSyDDAcQjBwIBA&url=http%3A%2F%2Frevelstokebearaware.org%2Fwp-content%2Fuploads%2FBlack-bear1.jpg&psig=AFQjCNEsD-fcjWag_44pO9C4EDOjiY27YA&ust=1495731641397930")
+var bear5 = new Bear('Bertha', "Brown", "http://thefederalist.com/wp-content/uploads/2016/06/shutterstock_115473676-998x665.jpg")
 var bear6 = new Bear('Fred', "Black", "http://highlighthollywood.com/wp-content/uploads/2016/06/blackbear-whitecub.jpg")
-var bear7 = new Bear('Cloonie', "Pink", "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwihnOvlgInUAhUJ5GMKHZH_DdwQjBwIBA&url=http%3A%2F%2Fwww.nationalgeographic.com%2Fcontent%2Fdam%2Fanimals%2Fpictures%2Fmammals%2Fa%2Famerican-black-bear%2Famerican-black-bear.JPG&psig=AFQjCNF7XOLfTPWrDqal1BtMqwbPIqmLKg&ust=1495731662092996")
-var bear8 = new Bear('Sam', "Purple", "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwj5ttDqgInUAhVDw2MKHWorD3EQjBwIBA&url=https%3A%2F%2Fwww.bear.org%2Fwebsite%2Fimages%2Fstories%2Fimages%2Fimages-new%2Fblack_bear_w_5_cubs_nh_june_2007.jpg&psig=AFQjCNF7XOLfTPWrDqal1BtMqwbPIqmLKg&ust=1495731662092996")
+var bear7 = new Bear('Cloonie', "Pink", "http://www.gotscience.org/wp-content/uploads/2015/08/19718748235_cd5c821289_k-e1439850100575.jpg")
+var bear8 = new Bear('Sam', "Purple", "https://ichef.bbci.co.uk/news/624/media/images/76762000/jpg/_76762768_grizzlybear76993.jpg")
 var bear9 = new Bear('Charles', "Magent", "https://s-media-cache-ak0.pinimg.com/736x/08/67/e0/0867e0b750921f149f658f2a40808a18.jpg")
 
 var allBears = [bear, bear2, bear3, bear4, bear5, bear6,
                 bear7, bear8, bear9];
 
-console.log(allBears[3]);
+var newBearVue = new Vue({
+  el: '#bear',
+  data: {
+    greeting: "Hello, from Bear Vue",
+    oneBear: allBears[0],
+    bears: allBears
+  }
+})
